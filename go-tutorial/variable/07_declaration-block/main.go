943540776ef5fe4b6d52874f512b1686c05f4b0c
gaochangliang <golanggo@163.com>
2021-03-26 15:02:20 +0800

    add go tutorial variable

---
+++ go-tutorial/variable/07_declaration-block/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+var (
+	productName = "RTX3080"
+	quantity    = 100
+	price       = 5499
+	inStock     = true
+)
+
+func main() {
+	fmt.Println(productName)
+	fmt.Println(quantity)
+	fmt.Println(price)
+	fmt.Println(inStock)
+}
