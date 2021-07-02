94031c08c4814cd5648f5fbb4b8efdc0bbbb31a3
gaochangliang <golanggo@163.com>
2021-07-02 18:14:05 +0800

    update 上传图片

--- 01_go-tutorial/36-upload-image/01_upload-image/routers/api/upload.go
+++ 01_go-tutorial/36-upload-image/01_upload-image/routers/api/upload.go
@@ -1,1 +1,25 @@
 package api
+
+import (
+	"example/uploadimage/pkg/e"
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+)
+
+func uploadImage(c *gin.Context) {
+	code := e.SUCCESS
+	data := make(map[string]string)
+
+	file, image, err := c.Request.FormFile("image")
+	if err != nil {
+		log.Printf("c.Request.FormFile err: %s\n", err)
+		code = e.ERROR
+		c.JSON(http.StatusOK, gin.H{
+			"code": code,
+			"msg":  e.GetMsg(code),
+			"data": data,
+		})
+	}
+
+}
