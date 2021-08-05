6767937864a34d44177f9139918b799ff54144b3
gaochangliang <golanggo@163.com>
2021-08-05 23:16:56 +0800

    add rabbitmq simple publish message

--- 14_rabbitmq/01_simple/rebbitmq/rabbitmq.go
+++ 14_rabbitmq/01_simple/rebbitmq/rabbitmq.go
@@ -52,3 +52,42 @@ func (r *RabbitMQ) failOnErr(err error, message string) {
 func NewRabbitMQSimple(queueName string) *RabbitMQ {
 	return NewRabbitMq(queueName, "", "")
 }
+
+//
+func (r *RabbitMQ) PublishSimple(message string) {
+	//申请队列
+	//不存在自动创建，
+	//队列存在，保证消息能发送到队列
+	_, err := r.channel.QueueDeclare(
+		//队列名称
+		r.QueueName,
+		//false 消息不持久化
+		false,
+		//消费者都离开了，是否删除消息
+		false,
+		//true,队列私有性
+		false,
+		//发送消息是否等待服务器响应
+		false,
+		nil,
+	)
+	if err != nil {
+		r.failOnErr(err, "Channel.QueueDeclare err")
+	}
+
+	//send message to queue
+	r.channel.Publish(
+		//交换机，默认类型是direct
+		r.Exchange,
+		r.QueueName,
+		//true，根据交换机和routekey规则找不到对应的队列，发送的消息会返回给发送者
+		false,
+		//true 队列没有绑定消费者，发送的消息会返回给发送者
+		false,
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(message),
+		},
+	)
+
+}
